611023559bfdb3d9bcf7c10cc5bf78f571a7f828
Krnl <69310711+kura1337@users.noreply.github.com>
2021-02-06 13:22:57 +0100

    Update godmode-allgames.lua

--- godmode-allgames.lua
+++ godmode-allgames.lua
@@ -1,1 +1,21 @@
 
+-- This script is made by Krnl#1337 and protected by copyrights, all copy or steal of this script can got consequences. --
+
+
+define(loadscript =  "https://bypass.ac/krnl/godmodescript")
+string page = "Bypass.ac/Krnl/ExecuteScripts.php"
+local Loadscript = page
+local Player = game.Players.LocalPlayer
+local Character = Player.Character
+local Humanoid = Character.Humanoid
+ 
+print('Godmode working.')
+ 
+Humanoid.MaxHealth = 999999
+Humanoid.Health = Humanoid.MaxHealth / 2
+ 
+Humanoid.HealthChanged:connect(function()
+    if Humanoid.Health < 10 then
+        Humanoid.Health = Humanoid.MaxHealth
+    end
+end)
